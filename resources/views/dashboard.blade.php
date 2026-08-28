f6a4af273a50f471510981d14da2545207e7281c
Farham BRIN <brin.farham@gmail.com>
2026-08-28 11:28:41 +0700

    Edit Lagi

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-sm-6">
-                    <h3 class="mb-0">Dashboard Edit</h3>
+                    <h3 class="mb-0">Dashboard Edit by {{ Auth::user()->name }}</h3>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-end">
