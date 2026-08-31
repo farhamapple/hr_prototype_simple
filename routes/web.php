9aaf566a87a845178ae2daa860fd0d10c615db45
Ryan <ryanhadi412003@gmail.com>
2026-08-31 13:14:44 +0700

    Menambahkan fitur data pekerjaan pada routes web

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\JobController;
-use App\Http\Controllers\RegionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
